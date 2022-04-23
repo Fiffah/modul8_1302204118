f2ab03ef1f1b5d6bb1feb9a19d3eb70183e0dd22
Fiffah <101395835+Fiffah@users.noreply.github.com>
2022-04-23 15:17:10 +0700

    Menambahkan API Movies

--- modul8_1302204118/Controllers/MovieController.cs
+++ modul8_1302204118/Controllers/MovieController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace modul8_1302204118.Controllers
 {
-    public class MoviesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : ControllerBase
     {
-        public IActionResult Index()
+        private static List<string> Stars1 = new List<string>
         {
-            return View();
+            "Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"
+        };
+        private static List<string> Stars2 = new List<string>
+        {
+            "Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"
+        };
+        private static List<string> Stars3 = new List<string>
+        {
+            "Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine"
+        };
+
+        private readonly ILogger<MovieController> _logger;
+
+        List<Movies> movies2 = new List<Movies>();
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
         }
+
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            movies2.Add(new Movies(" The Shawshank Redemption", "Frank Darabont", Stars1, "Two imprisoned men bond over a number of years"));
+            movies2.Add(new Movies(" The Godfather", "Francis Ford Coppola", Stars2, "The aging patriarch of an organized crime dynasty"));
+            movies2.Add(new Movies(" The Dark Knight", "Christopher Nolan", Stars3, "When the menace known as the Joker wreaks havoc an chaos"));
+
+
+            return movies2.ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public Movies GetMoviesById(int id)
+        {
+            movies2.Add(new Movies(Movies.Title, Movies.Director, Movies.Stars, Movies.Description));
+            return movies2;
+        }
+
     }
 }
--- modul8_1302204118/Movies.cs
+++ modul8_1302204118/Movies.cs
@@ -2,6 +2,19 @@
 {
     public class Movies
     {
+        private string v1;
+        private string v2;
+        private List<string> stars2;
+        private string v3;
+
+        public Movies(string v1, string v2, List<string> stars2, string v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.stars2 = stars2;
+            this.v3 = v3;
+        }
+
         private string Title { get; set; }
         private string Director { get; set; }
         private List<string> Stars { get; set; }
