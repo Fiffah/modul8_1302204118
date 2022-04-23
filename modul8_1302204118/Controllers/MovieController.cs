ed17dc6b10641512dc87f2df12d3fd7bcd7abdf7
Fiffah <101395835+Fiffah@users.noreply.github.com>
2022-04-23 14:31:07 +0700

    first commit

---
+++ modul8_1302204118/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul8_1302204118.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
